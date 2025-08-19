93fb9d0de375e11d3981a7a786f066c3be6db4a9
Abishaa <abiramdil@gmail.com>
2025-08-19 14:21:48 +0530

    Single Inheritance class

---
+++ SampleProject/src/singleinheritance_example/SlInheritanceDemo.java
@@ -0,0 +1,12 @@
+package singleinheritance_example;
+
+
+public class SlInheritanceDemo 
+{
+	public static void main(String[] args) 
+	{
+		Trainer t=new Trainer(112,"Abishaa","Puducherry",52,"Lead","Puducherry");
+				System.out.println(t);;
+	}
+	}
+
